d1dc9ea61c08a285d30b058f404531c1f52d6b22
Scott Grams <scott.grams@greenheck.com>
2019-11-28 18:26:07 -0600

    Create Build: Setting ApplicationVersion on the Build as a new Class is selected from the drop-down

--- ChroniCalc/MainForm.cs
+++ ChroniCalc/MainForm.cs
@@ -804,6 +804,7 @@ namespace ChroniCalc
                 //Update data on the build (everything not listed here was handled in the ResetCharacter() code (e.g. level, masteryLevel, trees, skills, etc)
                 build.characterClass = selectedClass;
                 build.name = "<Unnamed>";
+                build.ApplicationVersion = Application.ProductVersion;
 
                 InitializeBuild(build);
 
